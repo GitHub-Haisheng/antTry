cf11df238509cd7a3173fb4354bbcb9e4c66a65c
wb-chenamin <wb-chenamin@alibaba-inc.com>
2016-12-01 11:34:44 +0800

    css modify

--- dva-quickstart/src/components/MenuConfig/MenuConfig.js
+++ dva-quickstart/src/components/MenuConfig/MenuConfig.js
@@ -39,28 +39,28 @@ const MainLayout = React.createClass({
             <Link to="/completed">Completed</Link><br />
             <Link to="/404">404</Link><br />*/}
             <Menu theme={this.state.theme}
-              onClick={this.handleClick}
-              style={{ width: 300 }}
-              defaultOpenKeys={['sub1','sub2','sub3']}
-              selectedKeys={[this.state.current]}
-              mode="inline"
+                onClick={this.handleClick}
+                style={{ width: 224 }}
+                defaultOpenKeys={['sub1','sub2','sub3']}
+                selectedKeys={[this.state.current]}
+                mode="inline"
             >
-              <SubMenu key="sub1" title={<span><Icon type="mail" /><span>first try</span></span>}>
-                <Menu.Item key="1"><Link to="/table"></Link>我的表格</Menu.Item>
-                <Menu.Item key="2"><Link to="/">我的表单</Link></Menu.Item>
-                <Menu.Item key="4"><Link to="/">还没定</Link></Menu.Item>
-                {/*<Menu.Item key="3"><Link to="/404">404</Link></Menu.Item>*/}
-              </SubMenu>
-              <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>whatever~</span></span>}>
-                <Menu.Item key="5"><Link to="/"></Link>还没定</Menu.Item>
-                <Menu.Item key="6">还没定</Menu.Item>
-                <Menu.Item key="7">还没定</Menu.Item>
-                <Menu.Item key="8">还没定</Menu.Item>
-              </SubMenu>
-              <SubMenu key="sub3" title={<span><Icon type="setting" /><span>bingo~</span></span>}>
-                <Menu.Item key="9">还没定</Menu.Item>
-                <Menu.Item key="10">还没定</Menu.Item>
-              </SubMenu>
+                <SubMenu key="sub1" title={<span><Icon type="mail" /><span>first try</span></span>}>
+                    <Menu.Item key="1"><Link to="/table"></Link>我的表格</Menu.Item>
+                    <Menu.Item key="2"><Link to="/">我的表单</Link></Menu.Item>
+                    <Menu.Item key="4"><Link to="/">还没定</Link></Menu.Item>
+                    {/*<Menu.Item key="3"><Link to="/404">404</Link></Menu.Item>*/}
+                </SubMenu>
+                <SubMenu key="sub2" title={<span><Icon type="appstore" /><span>whatever~</span></span>}>
+                    <Menu.Item key="5"><Link to="/"></Link>还没定</Menu.Item>
+                    <Menu.Item key="6">还没定</Menu.Item>
+                    <Menu.Item key="7">还没定</Menu.Item>
+                    <Menu.Item key="8">还没定</Menu.Item>
+                </SubMenu>
+                <SubMenu key="sub3" title={<span><Icon type="setting" /><span>bingo~</span></span>}>
+                    <Menu.Item key="9">还没定</Menu.Item>
+                    <Menu.Item key="10">还没定</Menu.Item>
+                </SubMenu>
             </Menu>
           </div>
           <div className={styles.main}>
